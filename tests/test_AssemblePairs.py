1c6d61a638fa68128b5bdc600916a86022c9397c
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-03-04 18:34:55 -0500

    Added TCR sequence to AssemblePairs unit tests.

--- tests/test_AssemblePairs.py
+++ tests/test_AssemblePairs.py
@@ -10,6 +10,7 @@ __date__      = '2014.12.11'
 
 # Imports
 import time, unittest
+import pandas as pd
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from IgCore import readSeqFile
@@ -20,7 +21,12 @@ class TestAssemblePairs(unittest.TestCase):
 
     def setUp(self):
         print '-> %s()' % self._testMethodName
-        self.ref_file = 'IMGT_Human_IGV.fasta'
+
+        # Set pandas options
+        pd.set_option('display.width', 120)
+
+        #self.ref_file = 'IMGT_Human_IGV.fasta'
+        self.ref_file = 'IG_TR.V.human.F+ORF+infrP.ungapped.fasta'
         self.ref_dict = {s.id:s.upper() for s in readSeqFile(self.ref_file)}
         # MISEQ:121:000000000-A7VDM:1:1101:10041:1280
         #self.head_seq = Seq("CCACGTTTTAGTAATTAATACGGGAGCAAAAACCAGGGAAAGCCCCTAAGCTCCTGCTCTATGCTGCATCCACTTTGCAAAGTGTGGTCCCATCACGGTTCAGCGGCAGTGGATCTGGGACAGAATTCACTCTCACAATCAGCAGCCTGCAGCCTGAAGATTTTGCAACTTATTACTGTCAACAGCTTACTCCTTACCCTCCTACGTTCGCCCCAGGCCCCACGGTCGACCTCCACCCCCCTCTCGCTGCCCCCTCTCTCCCCTCCGACCCGCCTC")
@@ -30,14 +36,18 @@ class TestAssemblePairs(unittest.TestCase):
         #self.head_seq = Seq("GGAGCTTGCATTAGCATCGATACGGGGAGCTGTGGGCTCAGAAGCAGAGTTCTGGGGTGTCTCCACCATGGCCTGGACCCCTCTCTGGCTCACTCTCCTCACTCTTTGCATAGGTTCTGTGGTTTCTTCTGAGCTGACTCAGGACCCTGCTGTGTCTGTGGCCTTGGGACAGACAGTCAGGATCACATGCCAAGGAGACCGCCTCAGAAGCTATTATGCAAGCTGGTCCCCGCAGAATCCAGGACAGGCCCCCCTCCTTCTCATCTATGGTATAAC")
         #self.tail_seq = Seq("CTTGGGACAGACAGTCAGGATCACAGGCCAAGGAGACAGGCTCAGAAGCTAGTATGCAAGCGGGTACCAGCAGAAGCCAGGACAGGCCCCTGTACTTGTCATCTATGGTAAAAACAACCGGCCCTCAGGGATCCCAGACCGATTCTCTGGCTTCAGCTCAGGAAACACAGCTTCCTTGACCATCACTGGGGCTCAGGCGGAAGATGAGGCTGACTATTACTGTAACTCCCGGGACAGCAGTGGTAACCATCCATTCGGCGGAGGGACCAAGCTGACCGTCCTAGGTCAGCCCAAGGCTGCCCCCTCGGTCACTCTGTTCCCACCCT")
 
-        # Non-Overlmodping MISEQ:121:000000000-A7VDM:1:1101:13900:1538
-        self.head_seq = Seq("GGAGCTTGCATTAGCATCGATACGGGGAGCTGTGGGCTCAGAAGCAGAGTTCTGGGGTGTCTCCACCATGGCCTGGACCCCTCTCTGGCTCACTCTCCTCACTCTTTGCATAGGTTCTGTGGTTTCTTCTGAGCTGACTCAGGACCCTGCTGTGTCTGTGGCCTTGGGACAGACAGTCAGGAC")
-        self.tail_seq = Seq("TTGTCATCTATGGTAAAAACAACCGGCCCTCAGGGATCCCAGACCGATTCTCTGGCTTCAGCTCAGGAAACACAGCTTCCTTGACCATCACTGGGGCTCAGGCGGAAGATGAGGCTGACTATTACTGTAACTCCCGGGACAGCAGTGGTAACCATCCATTCGGCGGAGGGACCAAGCTGACCGTCCTAGGTCAGCCCAAGGCTGCCCCCTCGGTCACTCTGTTCCCACCCT")
+        # Non-Overlaping MISEQ:121:000000000-A7VDM:1:1101:13900:1538
+        #self.head_seq = Seq("GGAGCTTGCATTAGCATCGATACGGGGAGCTGTGGGCTCAGAAGCAGAGTTCTGGGGTGTCTCCACCATGGCCTGGACCCCTCTCTGGCTCACTCTCCTCACTCTTTGCATAGGTTCTGTGGTTTCTTCTGAGCTGACTCAGGACCCTGCTGTGTCTGTGGCCTTGGGACAGACAGTCAGGAC")
+        #self.tail_seq = Seq("TTGTCATCTATGGTAAAAACAACCGGCCCTCAGGGATCCCAGACCGATTCTCTGGCTTCAGCTCAGGAAACACAGCTTCCTTGACCATCACTGGGGCTCAGGCGGAAGATGAGGCTGACTATTACTGTAACTCCCGGGACAGCAGTGGTAACCATCCATTCGGCGGAGGGACCAAGCTGACCGTCCTAGGTCAGCCCAAGGCTGCCCCCTCGGTCACTCTGTTCCCACCCT")
 
         # MISEQ:121:000000000-A7VDM:1:1101:22042:1446
         #self.head_seq = Seq("GTCTCAACAAAATACCATCTACGGGATGTGTATTGGTACCAGCAACTCCCAGGATCGGCCCCCAAGCTCCTCATTTATAGTAGTAGCCAGCCGCCCTCAGGGGTCCCTGACCGATTCTCTGGCTCCAAGTCTGGCACCTCAGCCTCCCTGGCCCTCCGTGTGCTCCGTTCCGAAGATGACGCCGCTTATTCCTGTTCAGCCTGGGCTTACCCCCTGATTCGTCTTTCCTTGTTCGCCCCACCGTCCCCTCCCCCCCTCCTACCTCCGCCCCACGCC")
         #self.tail_seq = Seq("GAGAGGTGGGAGGAGCCGATCTGGGTCAACAAAATAGGATAGACGGGAAGGGTATTGGTACAAGCAACTCCCAGGAGCGGAGCCCAAGCTCGTCATTTATAGGAGAAGCCAGGGGCCCTCAGGGGTACCTGACCGATTCTCTGGCTCCAAGTCTGGAAGCTAAGCCTCCCTGGCCATCAGTGGGCTCCGGTACGAAGATGAGGCTGATTATTACTGTGCAGCATGGGATGACAGCCTGAGTGGTCTTTGGGTGTTCGGCGGAGGGACCAGGCTGACCGTCCTAGGTCAGCCCAAGGCTGCCCCCTCGGTCACTCTGTTCCCACCCT")
 
+        # TATATTATGTGCAGT_GCCTTC|PRCONS=TRB
+        self.head_seq = Seq('gacacctctccccagagaaggtggtgtgagaccaccacggaagatgctgctgctactgctcctcctgtggataggctccgggcttggtgccgtcgtctctcaacatccgagccgggctatctgaaagtgtggaaccattgtcaaccacgagggccgttcaccggactttcggccccctacgaagttttggcaacctcagctcccgatacatggtcttttgctgctggcgacctccaaccggggctccac')
+        self.tail_seq = Seq('gtcgagtgtcgttccctggactttcaagccacatcgaagtactggtatcggcagttcccgaaacagagtctaaggctggtggcaacatccaacgagtactcaaaggccacatacgagcaaggcgtcgagaaggacaagtatctcatcaaccatgcaagcctgaccttgtcctctcttacagtgaccagtgcccagcctgaagacagcagcttctacatctacagtgctgcaacgggtcaggggacgatcgagacccagtatttcgggcctggcacgcggctcctggtgctcgaggacctgaaaaac')
+        #self.head_seq = self.head_seq.reverse_complement()
 
         self.head_rec = SeqRecord(self.head_seq, id='HEAD', name='HEAD', description='')
         self.tail_rec = SeqRecord(self.tail_seq, id='TAIL', name='TAIL', description='')
@@ -54,25 +64,38 @@ class TestAssemblePairs(unittest.TestCase):
     def test_getUBlastAlignment(self):
         head_df = mod.getUblastAlignment(self.head_rec, self.ref_file)
         tail_df = mod.getUblastAlignment(self.tail_rec, self.ref_file)
+        print 'HEAD SEQUENCE>'
         print head_df
+        print 'TAIL SEQUENCE>'
         print tail_df
         self.fail()
 
-    #@unittest.skip("-> getBlastnAlignment() skipped\n")
+    @unittest.skip("-> getBlastnAlignment() skipped\n")
     def test_getBlastnAlignment(self):
         head_df = mod.getBlastnAlignment(self.head_rec, self.ref_file)
         tail_df = mod.getBlastnAlignment(self.tail_rec, self.ref_file)
+        print 'HEAD SEQUENCE>'
         print head_df
+        print 'TAIL SEQUENCE>'
         print tail_df
         self.fail()
 
     #@unittest.skip("-> referenceAssembly() skipped\n")
     def test_referenceAssembly(self):
         stitch = mod.referenceAssembly(self.head_rec, self.tail_rec, self.ref_dict, self.ref_file)
+
+        print '   REFID> %s' % stitch.ref_seq.id
+        print '  REFSEQ> %s' % (' ' * stitch.ref_pos[0] + stitch.ref_seq.seq)
+        print 'ASSEMBLY> %s' % stitch.seq.seq
+        print '     GAP> %s' % stitch.gap
+        print ' EVALUE1> %.4e' % stitch.evalue[0]
+        print ' EVALUE2> %.4e' % stitch.evalue[1]
+        print 'IDENTITY> %.4f' % stitch.ident
+
         #print tuple(stitch.evalue)
         self.fail()
 
-    #@unittest.skip("-> alignAssembly() skipped\n")
+    @unittest.skip("-> alignAssembly() skipped\n")
     def test_alignAssembly(self):
         head = SeqRecord(Seq("TTTCCGG"), id="HEAD",
                          letter_annotations={'phred_quality':[40,40,40,20,20,40,40]})
