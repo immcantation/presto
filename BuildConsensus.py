05e094b95715f3e0105ed039bb95b86706366254
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-03-20 16:53:03 -0400

    Cleanup of dates, TODO, README and CHANGELOG before official tag of v0.4.5.

--- BuildConsensus.py
+++ BuildConsensus.py
@@ -7,7 +7,7 @@ __author__    = 'Jason Anthony Vander Heiden'
 __copyright__ = 'Copyright 2013 Kleinstein Lab, Yale University. All rights reserved.'
 __license__   = 'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported'
 __version__   = '0.4.5'
-__date__      = '2015.03.15'
+__date__      = '2015.03.20'
 
 # Imports
 import os, sys, textwrap
