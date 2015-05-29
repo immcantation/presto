ca766fc528b511c86f04a12adbd7f7b06ceae1b1
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2015-05-29 13:57:39 -0400

    Updated Sequence unit tests.

--- tests/test_Sequence.py
+++ tests/test_Sequence.py
@@ -17,26 +17,65 @@ __author__ = 'Jason Anthony Vander Heiden'
 class TestIgCore(unittest.TestCase):
     def setUp(self):
         print '-> %s()' % self._testMethodName
+
         # Test DNA sequences
         seq_dna = [Seq('CGGCGTAA'),
-                   Seq('CGNNGTAA'),
+                   Seq('CGNNGTAG'),
                    Seq('CGGC--AA'),
-                   Seq('CGNN--AA')]
+                   Seq('CGNN--AG'),
+                   Seq('NNNNNNNN'),
+                   Seq('NNNNNNAA'),
+                   Seq('--------'),
+                   Seq('CG------')]
         self.records_dna = [SeqRecord(s, id='SEQ%i' % i, name='SEQ%i' % i, description='')
                             for i, s in enumerate(seq_dna, start=1)]
-        self.weight_dna = [8, 6, 6, 4]
-        # scoreSeqPair returns (score, minimum weight, error rate)
-        # TODO:  this 0.3333 case is funny (SEQ2 vs SEQ3). fix is not obvious.
-        self.error_dna = [0.0, 0.0, 0.0, 0.3333, 0.0, 0.0]
 
-        #Test amino acid sequences
+        # Make sequence pairs
+        self.seq_pairs = list(itertools.combinations(self.records_dna, 2))
+
+        # Weights
+        self.weight_dna_mask = [8, 6, 8, 6, 0, 2, 8, 8]
+
+        # Error rates
+        # scoreSeqPair returns (score, minimum weight, error rate)
+        # Asymmetric case is with score_dict = getDNAScoreDict(n_score=(0, 1), gap_score=(0, 1))
+        # 1vs2, 1vs3, 1vs4, 1vs5, 1vs6, 1v7, 1v8
+        # 2vs3, 2vs4, 2vs5, 2vs6, 2v7, 2v8,
+        # 3vs4, 3vs5, 3vs6, 3vs7, 3v8,
+        # 4vs5, 4vs6, 4vs7, 4vs8,
+        # 5vs6, 5vs7, 5v8,
+        # 6vs7, 6vs8,
+        # 7vs8
+        self.error_dna_def = [1.0/8, 2.0/8, 3.0/8, 0.0/8.0, 0.0/8.0, 8.0/8, 6.0/8,
+                              3.0/8, 2.0/8, 0.0/8, 1.0/8, 8.0/8, 6.0/8,
+                              1.0/8, 2.0/8, 2.0/8, 6.0/8, 4.0/8,
+                              2.0/8, 3.0/8, 6.0/8, 4.0/8,
+                              0.0/8, 8.0/8, 6.0/8,
+                              8.0/8, 6.0/8,
+                              2.0/8]
+        self.error_dna_asym = [1.0/8, 0.0/8, 1.0/8, 0.0/8.0, 0.0/8.0, 0.0/8.0, 0.0/8.0,
+                               3.0/8, 2.0/8, 2.0/8, 3.0/8, 2.0/8, 2.0/8,
+                               3.0/8, 2.0/8, 2.0/8, 2.0/8, 2.0/8,
+                               4.0/8, 5.0/8, 4.0/8, 4.0/8,
+                               8.0/8, 8.0/8, 8.0/8,
+                               6.0/8, 6.0/8,
+                               8.0/8]
+        self.error_dna_mask = [1.0/6, 2.0/8, 3.0/6, 1.0, 0.0/2, 8.0/8, 6.0/8,
+                               3.0/6, 2.0/6, 1.0, 1.0/2, 6.0/6, 4.0/6,
+                               1.0/6, 1.0, 0.0/2, 6.0/8, 4.0/8,
+                               1.0, 1.0/2, 4.0/6, 2.0/6,
+                               1.0, 1.0, 1.0,
+                               2.0/2, 2.0/2,
+                               2.0/8]
+
+        # Test amino acids sequences
         seq_aa = [Seq('PQRRRWQQ'),
                   Seq('PQXRRWQX'),
                   Seq('PQR--WQQ'),
                   Seq('PQX--WQX')]
         self.records_aa = [SeqRecord(s, id='SEQ%i' % i, name='SEQ%i' % i, description='')
                            for i, s in enumerate(seq_aa, start=1)]
-        self.weight_aa = [8, 6, 6, 4]
+        self.weight_aa_mask = [8, 6, 8, 6]
 
         # Test DNA characters
         self.pairs_dna_chars = [('A', 'A'),
@@ -61,7 +100,6 @@ class TestIgCore(unittest.TestCase):
         # Character pair scores for asymmetric case where N/gaps in character one are a mismatch
         self.pairs_scores_asym = [1, 0, 1, 0, 1, 0]
 
-
         # Start clock
         self.start = time.time()
 
@@ -70,45 +108,80 @@ class TestIgCore(unittest.TestCase):
         t = time.time() - self.start
         print '<- %s() %.3f' % (self._testMethodName, t)
 
-    #@unittest.skip('-> weightDNA() skipped\n')
+    #@unittest.skip('-> weightSeq() skipped\n')
     def test_weightDNA(self):
-        scores = [presto.Sequence.weightDNA(x) for x in self.records_dna]
+        # DNA weights
+        ignore_chars = set(['n', 'N'])
+        weights = [weightSeq(x, ignore_chars=ignore_chars) for x in self.records_dna]
         print 'DNA Weight>'
-        for x, s in zip(self.records_dna, scores):
+        for x, s in zip(self.records_dna, weights):
             print '  %s> %s' % (x.id, s)
 
-        self.assertEqual(scores, self.weight_dna)
+        self.assertSequenceEqual(weights, self.weight_dna_mask)
 
-    #@unittest.skip('-> weightAA() skipped\n')
-    def test_weightAA(self):
-        scores = [presto.Sequence.weightAA(x) for x in self.records_aa]
+        # Amino acid weights
+        ignore_chars = set(['x', 'X'])
+        weights = [weightSeq(x, ignore_chars=ignore_chars) for x in self.records_aa]
         print 'AA Weight>'
-        for x, s in zip(self.records_aa, scores):
+        for x, s in zip(self.records_dna, weights):
             print '  %s> %s' % (x.id, s)
 
-        self.assertEqual(scores, self.weight_aa)
+        self.assertSequenceEqual(weights, self.weight_aa_mask)
+
 
     #@unittest.skip('-> scoreSeqPair() skipped\n')
     def test_scoreSeqPair(self):
-        pairs = list(itertools.combinations(self.records_dna, 2))
-        scores = [presto.Sequence.scoreDNASeqPair(x, y) for x, y in pairs]
-        print 'DNA Scores>'
-        for (x, y), s in zip(pairs, scores):
-            print '  %s/%s> %s' % (x.id, y.id, s)
+        # Default scoring
+        scores = [scoreSeqPair(x, y) for x, y in self.seq_pairs]
+        print 'Default DNA Scores>'
+        for (x, y), s in zip(self.seq_pairs, scores):
+            print '    %s> %s' % (x.id, x.seq)
+            print '    %s> %s' % (y.id, y.seq)
+            print '   SCORE> %i' % s[0]
+            print '  WEIGHT> %i' % s[1]
+            print '   ERROR> %f\n' % s[2]
 
-        self.assertEqual([round(s[2], 4) for s in scores],
-                         [round(s, 4) for s in self.error_dna])
+        self.assertSequenceEqual([round(s[2], 4) for s in scores],
+                                 [round(s, 4) for s in self.error_dna_def])
+
+        # Asymmetric scoring without position masking
+        score_dict = getDNAScoreDict(n_score=(0, 1), gap_score=(0, 1))
+        scores = [scoreSeqPair(x, y, score_dict=score_dict) for x, y in self.seq_pairs]
+        print 'Asymmetric DNA Scores>'
+        for (x, y), s in zip(self.seq_pairs, scores):
+            print '    %s> %s' % (x.id, x.seq)
+            print '    %s> %s' % (y.id, y.seq)
+            print '   SCORE> %i' % s[0]
+            print '  WEIGHT> %i' % s[1]
+            print '   ERROR> %f\n' % s[2]
+
+        self.assertSequenceEqual([round(s[2], 4) for s in scores],
+                                 [round(s, 4) for s in self.error_dna_asym])
+
+        # Symmetric scoring with N positions excluded
+        ignore_chars = set(['n', 'N'])
+        scores = [scoreSeqPair(x, y, ignore_chars=ignore_chars) for x, y in self.seq_pairs]
+        print 'Masked DNA Scores>'
+        for (x, y), s in zip(self.seq_pairs, scores):
+            print '    %s> %s' % (x.id, x.seq)
+            print '    %s> %s' % (y.id, y.seq)
+            print '   SCORE> %i' % s[0]
+            print '  WEIGHT> %i' % s[1]
+            print '   ERROR> %f\n' % s[2]
+
+        self.assertSequenceEqual([round(s[2], 4) for s in scores],
+                                 [round(s, 4) for s in self.error_dna_mask])
 
     #@unittest.skip('-> scoreDNA() skipped\n')
     def test_scoreDNA(self):
-        scores = [presto.Sequence.scoreDNA(a, b) for a, b in self.pairs_dna_chars]
+        scores = [scoreDNA(a, b) for a, b in self.pairs_dna_chars]
         print 'Default DNA Scores>'
         for (a, b), s in zip(self.pairs_dna_chars, scores):
             print '  %s==%s> %s' % (a, b, s)
 
         self.assertSequenceEqual(self.pairs_scores_def, scores)
 
-        scores = [presto.Sequence.scoreDNA(a, b, n_score=(1, 1), gap_score=(1, 1)) \
+        scores = [scoreDNA(a, b, n_score=(1, 1), gap_score=(1, 1)) \
                   for a, b in self.pairs_dna_chars]
         print 'Symmetric DNA Scores>'
         for (a, b), s in zip(self.pairs_dna_chars, scores):
@@ -116,7 +189,7 @@ class TestIgCore(unittest.TestCase):
 
         self.assertSequenceEqual(self.pairs_scores_sym, scores)
 
-        scores = [presto.Sequence.scoreDNA(a, b, n_score=(0, 1), gap_score=(0, 1)) \
+        scores = [scoreDNA(a, b, n_score=(0, 1), gap_score=(0, 1)) \
                   for a, b in self.pairs_dna_chars]
         print 'Asymmetric DNA Scores>'
         for (a, b), s in zip(self.pairs_dna_chars, scores):
