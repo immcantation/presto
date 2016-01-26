944bf7223daabbc2df0f4286743f8fe619cd2da3
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2016-01-26 12:51:22 -0500

    You're killing me, readthedocs. You're killing me.

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'pandas', 'Bio']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.stats', 'pandas',
+                'Bio', 'Bio.Align', 'Bio.Alphabet', 'Bio.Seq', 'Bio.SeqRecord']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
