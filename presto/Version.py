82706aae7ef7fb3326f9ca78249a21a5316cfbf5
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2017-05-05 13:53:20 -0400

    Bumped to development version.

--- presto/Version.py
+++ presto/Version.py
@@ -4,5 +4,5 @@ Version and authorship information
 __author__    = 'Jason Anthony Vander Heiden'
 __copyright__ = 'Copyright 2017 Kleinstein Lab, Yale University. All rights reserved.'
 __license__   = 'Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)'
-__version__   = '0.5.3'
-__date__      = '2017.02.14'
+__version__   = '0.5.3.999'
+__date__      = '2017.05.05'
