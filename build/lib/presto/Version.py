577c641287c895103c0e29e8cf05cd058d191d6e
ruoyijiangyale <ruoyi.jiang@yale.edu>
2018-07-02 14:57:28 -0400

    test_EstimateError.py

--- build/lib/presto/Version.py
+++
@@ -1,8 +0,0 @@
-"""
-Version and authorship information
-"""
-__author__    = 'Jason Anthony Vander Heiden'
-__copyright__ = 'Copyright 2017 Kleinstein Lab, Yale University. All rights reserved.'
-__license__   = 'Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)'
-__version__   = '0.5.7.999'
-__date__      = '2018.03.26'
