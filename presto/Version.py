681617415ce5e6c47ef82ee5c185195a8b88bb7e
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2020-09-27 12:04:04 -0700

    Bump version and date

--- presto/Version.py
+++ presto/Version.py
@@ -5,5 +5,5 @@ Version and authorship information
 __author__    = 'Jason Anthony Vander Heiden'
 __copyright__ = 'Copyright 2020 Kleinstein Lab, Yale University. All rights reserved.'
 __license__   = 'GNU Affero General Public License 3 (AGPL-3)'
-__version__   = '0.6.1'
-__date__      = '2020.07.15'
+__version__   = '0.6.1.999'
+__date__      = '2020.09.27'
