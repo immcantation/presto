697043e482c48e7957884e77ed450b7282985f5a
Susanna Marquez <susanna.marquez@yale.edu>
2023-08-07 17:16:28 +0200

    updates for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.napoleon',
               'sphinx.ext.todo',
-              'sphinxcontrib.autoprogram']
+              'sphinxcontrib.autoprogram',
+              'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
