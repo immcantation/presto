1630c59c369bc08c47bfb7ff7a5f0996590e33ea
Edel Aron <edel.aron@yale.edu>
2024-11-04 14:31:17 -0500

    removed abstract deadline, docs rebuild

--- docs/conf.py
+++ docs/conf.py
@@ -38,9 +38,8 @@ rst_prolog = """
     *Are you an Immcantation user and/or interested in adaptive immune receptor repertoire analysis?*
     
     Register now for the upcoming Immcantation Users Group Meeting!
-    
-    It will be held virtually on **January 30th, 2025**, from 10 to 1:30pm (ET).
-    All talks will be from user-submitted abstracts (due on **November 1st, 2024**).
+    It will be held virtually on **January 30th, 2025, from 10 to 1:30pm (ET)**.
+    All talks will be from user-submitted abstracts.
 
     Full information here: https://immcantation.github.io/users-meeting/
 """
