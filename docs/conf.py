e8f5f0d83165a6d364d6db1a0ad9cb4cd1498b9c
Susanna Marquez <susanna.marquez@yale.edu>
2024-11-12 10:38:43 +0100

    added GA tag

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,9 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'analytics_id': 'G-Q07WDQD80L'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
