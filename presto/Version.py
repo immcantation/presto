ca02cfc43326f1fad5d14907aab79cb4082c9e83
Susanna Marquez <susanna.marquez@yale.edu>
2025-03-10 16:28:43 +0100

    updated copyright

--- presto/Version.py
+++ presto/Version.py
@@ -3,7 +3,7 @@ Version and authorship information
 """
 
 __author__    = 'Jason Anthony Vander Heiden'
-__copyright__ = 'Copyright 2021 Kleinstein Lab, Yale University. All rights reserved.'
+__copyright__ = 'Copyright 2025 Kleinstein Lab, Yale University. All rights reserved.'
 __license__   = 'GNU Affero General Public License 3 (AGPL-3)'
 __version__   = '0.7.5dev'
 __date__      = '2025.03.10'
